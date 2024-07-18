56963662d9fa5a80a1c4fc3d6de400c9705b78ed
AbdulmalikSalawu <salawuabdulmalik90@gmail.com>
2024-07-18 19:08:45 +0100

    fixed some bugs

--- src/Components/NewCollections.jsx
+++ src/Components/NewCollections.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import '../Styles/NewCollections.css'
-import productData from './Data'
+// import productData from './Data'
 import Items from './Items'
 
 const NewCollections = () => {
+
+  const [productData,setProductData] = useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:4000/newCollections")
+    .then((response)=>response.json())
+    .then((data)=>setProductData(data))
+  },[])
+
   return (
     <div className='collections'>
         <h2 className='mt-3'>NEW COLLECTIONS</h2>
         <hr />
         <div className='collections-item'>
             {productData.filter((item)=>item.id>7).map((item,i)=>{
-                return <Items key={i} id={item.id} name={item.name} image={item.image} newPrice={item.newPrice} oldPrice={item.oldPrice}/>
+                return <Items key={i} id={item.id} name={item.name} image={item.newImage} newPrice={item.newPrice} oldPrice={item.oldPrice}/>
             })}
         </div>
     </div>
