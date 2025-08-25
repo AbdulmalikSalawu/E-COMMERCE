3394ba982319118e01e2c460853ccf997f7c532a
Abdulmalik <salawuabdulmalik90@gmail.com>
2025-08-24 20:21:33 -0500

    added paypal payment gateway

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,116 +1,172 @@
-import React, { useContext,useEffect, useState } from 'react'
-import "../Styles/Cart.css"
-import { ShopContext } from '../Context/ShopContext'
-import remove from "../Assets/remove.svg"
-import Navbar from './Navbar'
+import React, { useContext, useEffect, useState } from "react";
+import "../Styles/Cart.css";
+import { ShopContext } from "../Context/ShopContext";
+import remove from "../Assets/remove.svg";
+import Navbar from "./Navbar";
 // import productData from './Data'
 // import plus from '../Assets/plus-square.svg'
 // import dash from '../Assets/dash-square.svg'
 // import arrow from '../Assets/arrow-left.svg'
 // import carti from '../Assets/cart2.svg'
-import { useNavigate } from 'react-router'
-import axios from 'axios'
+import { useNavigate } from "react-router";
+import axios from "axios";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 const Cart = () => {
-    const {getTotalAmount,productData,cartItems,removeFromCart} = useContext(ShopContext)
-    const navigate = useNavigate()
-    const [email,setEmail] = useState("youremail@gmail.com")
-    const [userData,setUserData] = useState("")
+  const { getTotalAmount, productData, cartItems, removeFromCart } =
+    useContext(ShopContext);
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("youremail@gmail.com");
+  const [userData, setUserData] = useState("");
 
-    useEffect(() => {
-        fetch("https://storeformalik.onrender.com/userData", {
-        // fetch("http://localhost:4000/userData", {
-          method: "POST",
-          crossDomain: true,
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-          },
-          body: JSON.stringify({
-            token:localStorage.getItem("token"),
-          }),
-        })
-        .then((res)=>res.json())
-        .then((data)=> {
-            console.log(data, "userData")
-            setUserData(data.data)
-            setEmail(data.data.email)
-            if (data.data=="token expired") {
-              alert("session expired, login again");
-              localStorage.clear();
-              navigate('/login')
-            }
-          })
-      }, [])
-
-    const paywithpaystack = () => {
-        const totalCost = getTotalAmount()
-        if(localStorage.getItem('token')){
-        axios.post(`https://abdulmalikyinka.onrender.com/paywithpaystack`,{totalCost,email})
-        .then((res)=>{
-              let data = res.data
-              window.location.href=data.data.authorization_url
-        })} else {
-            navigate('/login')
+  useEffect(() => {
+    fetch("https://storeformalik.onrender.com/userData", {
+      // fetch("http://localhost:4000/userData", {
+      method: "POST",
+      crossDomain: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        token: localStorage.getItem("token"),
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "userData");
+        setUserData(data.data);
+        setEmail(data.data.email);
+        if (data.data == "token expired") {
+          alert("session expired, login again");
+          localStorage.clear();
+          navigate("/login");
         }
+      });
+  }, []);
+
+  const paywithpaystack = () => {
+    const totalCost = getTotalAmount();
+    if (localStorage.getItem("token")) {
+      axios
+        .post(`https://abdulmalikyinka.onrender.com/paywithpaystack`, {
+          totalCost,
+          email,
+        })
+        .then((res) => {
+          let data = res.data;
+          window.location.href = data.data.authorization_url;
+        });
+    } else {
+      navigate("/login");
     }
+  };
 
   return (
     <div>
-    <Navbar />
-    <div className='cartPage cart-container'>
+      <Navbar />
+      <div className="cartPage cart-container">
         <div className="mainCart mt-5 pt-3">
-            <p>Product</p>
-            <p>Title</p>
-            <p>Price</p>
-            <p>Quantity</p>
-            <p>Total</p>
-            <p>Remove</p>
+          <p>Product</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
         </div>
         <hr />
-        {productData.map((e)=>{
-            if(cartItems[e.id]>0) {
-            return  ( 
-                <div key={e.id}>
-                    <div className="cartFormat mainCart">
-                        <img src={e.newImage} alt="" className='productIcon' />
-                        <p className='name'>{e.name}</p>
-                        <p className='me-lg-5 price'>{e.newPrice}</p>
-                        <button className='quantity'>{cartItems[e.id]}</button>
-                        <p>{e.newPrice*cartItems[e.id]}</p>
-                        <img className='removeIcon' src={remove} onClick={()=>{removeFromCart(e.id)}} alt="" />
-                    </div>
-                    <hr />
-                </div>);
-            } return null
-        })}
-        
-        <div className="cartitems-down">
-            <div className="cartitems-total">
-                <h1>Cart Totals</h1>
-                <div>
-                    <div className="cartitems-total-item">
-                        <p>Subtotal</p>
-                        <p>${getTotalAmount()}</p>
-                    </div>
-                    <div className="cartitems-total-item">
-                        <p>Shipping Fee</p>
-                        <p>Free</p>
-                    </div>
-                    <hr />
-                    <div className="cartitems-total-item">
-                        <h3>Total</h3>
-                        <h3>${getTotalAmount()}</h3>
-                    </div>
+        {productData.map((e) => {
+          if (cartItems[e.id] > 0) {
+            return (
+              <div key={e.id}>
+                <div className="cartFormat mainCart">
+                  <img src={e.newImage} alt="" className="productIcon" />
+                  <p className="name">{e.name}</p>
+                  <p className="me-lg-5 price">{e.newPrice}</p>
+                  <button className="quantity">{cartItems[e.id]}</button>
+                  <p>{e.newPrice * cartItems[e.id]}</p>
+                  <img
+                    className="removeIcon"
+                    src={remove}
+                    onClick={() => {
+                      removeFromCart(e.id);
+                    }}
+                    alt=""
+                  />
                 </div>
-                <button onClick={paywithpaystack} className='checkout mt-4'>Proceed to Checkout</button>
-                {email}
-            </div>
-        </div>
-       
-    </div>
-    </div>
-  )
-}
+                <hr />
+              </div>
+            );
+          }
+          return null;
+        })}
 
-export default Cart
+        <div className="cartitems-down">
+          <div className="cartitems-total">
+            <h1>Cart Totals</h1>
+            <div>
+              <div className="cartitems-total-item">
+                <p>Subtotal</p>
+                <p>${getTotalAmount()}</p>
+              </div>
+              <div className="cartitems-total-item">
+                <p>Shipping Fee</p>
+                <p>Free</p>
+              </div>
+              <hr />
+              <div className="cartitems-total-item">
+                <h3>Total</h3>
+                <h3>${getTotalAmount()}</h3>
+              </div>
+            </div>
+            <button onClick={paywithpaystack} className="checkout mt-4">
+              Pay with paystack
+            </button>
+
+            <div className="cartitems-total">
+              {/* PayPal Integration */}
+              <PayPalScriptProvider
+                options={{
+                  "client-id":
+                    "AQKQkWTppdFkGwobtEgj5PDeeUETPCqL30PcFloGBqoCH2jbmZp9JffWacP2jGbcRGDsjOh7MeZbP3sm",
+                }}
+              >
+                <PayPalButtons
+                  className="mt-4"
+                  style={{ layout: "vertical" }}
+                  createOrder={(data, actions) => {
+                    return actions.order.create({
+                      purchase_units: [
+                        {
+                          amount: {
+                            value: getTotalAmount().toString(), // Total amount as string
+                          },
+                        },
+                      ],
+                    });
+                  }}
+                  onApprove={(data, actions) => {
+                    return actions.order.capture().then((details) => {
+                      alert(
+                        "Transaction completed by " +
+                          details.payer.name.given_name
+                      );
+                      // You can also send details to your backend here
+                    });
+                  }}
+                  onError={(err) => {
+                    alert("PayPal Checkout Error: " + err);
+                  }}
+                />
+              </PayPalScriptProvider>
+            </div>
+
+            {email}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
